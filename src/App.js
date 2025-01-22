0312f4644cfe9ad4f4e69ff18614a08d4b01bb6e
黒川颯 <ryu@kurokawasatsunoMacBook-Pro.local>
2025-01-22 18:56:10 +0900

    pokemon

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon"
+
+  useEffect(()=>{},{})
+
+
+
+  return (
+    <div className="App"></div>
+  );
+}
+
+export default App;
