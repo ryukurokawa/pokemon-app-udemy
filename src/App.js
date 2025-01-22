7b46f742096a169eef790f0292c031b8154c8ea1
黒川颯 <ryu@kurokawasatsunoMacBook-Pro.local>
2025-01-22 19:36:04 +0900

    pokemon-data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-
 import { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const initialURL = "https://pokeapi.co/api/v2/pokemon"
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon';
 
-  useEffect(()=>{},{})
+  useEffect(() => {
+    const fetchPokemomData = async () => {
+      //全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+    };
+  }, []);
 
-
-
-  return (
-    <div className="App"></div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
