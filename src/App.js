2454069e40203f50ccd5772a8488d28578320e9e
黒川颯 <ryu@kurokawasatsunoMacBook-Pro.local>
2025-01-23 20:06:30 +0900

    ポケモンcss components/

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import { useEffect } from 'react';
+import { use, useEffect, useState } from 'react';
 import './App.css';
+import Card from './components/Card/Card';
+import { getAllPokemon, getPokemon } from './utils/pokemon.js';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemomData = async () => {
       //全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
+      //各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+      setLoading(false);
     };
+    fetchPokemomData();
   }, []);
 
-  return <div className="App"></div>;
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <>
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
